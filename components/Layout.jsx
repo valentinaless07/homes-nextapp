0eb0ea1cd717ac673bf64ffc3d923312f5c18701
Valentín Alessandrini Zapata <79989652+valentinaless07@users.noreply.github.com>
2022-09-23 17:24:53 -0300

    added Layout component

---
+++ components/Layout.jsx
@@ -0,0 +1,29 @@
+import { Box } from "@chakra-ui/react";
+import Head from "next/head";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+const Layout = ({children}) => (
+    <>
+        <Head>
+            <title>Homes</title>
+        </Head>
+
+        <Box maxWidth="1280px" m="auto">
+            <header>
+                <Navbar/>
+            </header>
+
+            <main>
+                {children}
+            </main>
+
+            <footer>
+                <Footer/>
+            </footer>
+        </Box>
+
+    </>
+)
+
+export default Layout
