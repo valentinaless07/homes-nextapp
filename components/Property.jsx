4053268cce9cbee54d21a8c36c20bec38671ac29
Valentín Alessandrini Zapata <79989652+valentinaless07@users.noreply.github.com>
2022-09-25 22:39:16 -0300

    added Property info

--- components/Property.jsx
+++ components/Property.jsx
@@ -9,9 +9,29 @@ import DefaultImage from "../assets/images/houses.jpg"
 
 const Property = ({property: { price, coverPhoto, rentFrequency, rooms, title, baths, area, agency, isVerified, externalID}}) => (
     <Link href={`/property/${externalID}`} passHref>
-        <Flex flexWrap="wrap" w="420px" p="5" paddingTop="0" justifyContent="flex-start" cursor="pointer">
+        <Flex flexWrap="wrap" w="420px" p="5" paddingTop="0" margin="5" justifyContent="flex-start" cursor="pointer" boxShadow={"0 0.125rem 0.9375rem rgb(0 0 0 / 15%)"}>
             <Box>
-                <Image src={coverPhoto ? coverPhoto.url : DefaultImage} width={400} height={260} alt="apartment" />
+                <Image src={coverPhoto ? coverPhoto.url : DefaultImage} width={400} height={260} alt="property" />
+            </Box>
+
+            <Box w="full">
+                <Flex paddingTop="2" alignItems="center" justifyContent="space-between">
+                    <Flex alignItems="center">
+                        <Box paddingRight="3" color="green.400">{isVerified && <GoVerified/>}</Box>
+                        <Text fontWeight="bold" fontSize="lg">AED {millify(price)}{rentFrequency && `/${rentFrequency}`}</Text>
+                    </Flex>
+                    <Box>
+                        <Avatar size="sm" src={agency?.logo?.url}/>
+                    </Box>
+                </Flex>
+
+                <Flex alignItems="center" p="1" justifyContent="space-between" w="250px" color="blue.400">
+                    {rooms} <FaBed /> | {baths} <FaBath/> | {millify(area)} sqft <BsGridFill/>
+                </Flex>
+
+                <Text fontSize="lg" >
+                    {title.length > 30 ? `${title.substring(0, 30)}...` : title}
+                </Text>
             </Box>
         </Flex>
     </Link>
