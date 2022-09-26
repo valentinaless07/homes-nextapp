955cf4dda4c78d0794b9bc0e5598a106c7ae8c54
Valentín Alessandrini Zapata <79989652+valentinaless07@users.noreply.github.com>
2022-09-25 22:36:31 -0300

    github added to footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,8 @@
 import { Box } from "@chakra-ui/react";
 
 const Footer = () => (
-    <Box textAlign="center" p="5" color="gray.600" borderTop="1px" borderColor="gray.100">
-        2022 Homes App, Inc.
+    <Box textAlign="center" p="5" color="gray.600" borderTop="1px" borderColor="gray.100" marginTop="5">
+        2022 Homes, Inc. <a style={{textDecoration: "none"}} target="_blank" rel="noreferrer" href="https://github.com/valentinaless07">Github @valentinaless07</a>
     </Box>
 )
 
